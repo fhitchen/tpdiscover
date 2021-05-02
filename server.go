230820e9248edb047274ee75c84c627d8e5be770
EC2 Default User <ec2-user@ip-172-31-33-207.us-east-2.compute.internal>
2021-05-02 13:07:59 +0000

    Added TUXEDO server.

---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	atmi "github.com/endurox-dev/endurox-go"
+        "fmt"
+        "os"
+)
+
+//Service func
+//Here svc contains the caller infos
+func TPDISCOVER(ac *atmi.ATMICtx, svc *atmi.TPSVCINFO) {
+
+
+        ac.TpReturn(atmi.TPSUCCESS, 0, &svc.Data, 0)
+
+}
+
+//Server boot/init
+func Init(ac *atmi.ATMICtx) int {
+
+        //Advertize TPDISCOVER
+        if err := ac.TpAdvertise("TPDISCOVER", "TPDISCOVER", TPDISCOVER); err != nil {
+                fmt.Println(err)
+                return atmi.FAIL
+        }
+
+        return atmi.SUCCEED
+}
+
+//Server shutdown
+func Uninit(ac *atmi.ATMICtx) {
+        fmt.Println("Server shutting down...")
+}
+
+
+//Server main
+func main() {
+        //Have some context
+        ac, err := atmi.NewATMICtx()
+
+        if nil != err {
+                fmt.Errorf("Failed to allocate cotnext!", err)
+                os.Exit(atmi.FAIL)
+        } else {
+                //Run as server
+                ac.TpRun(Init, Uninit)
+        }
+}
+
